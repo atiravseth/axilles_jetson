dab6fb7cdb4e818a1d50ad4990b33f3df0716353
atiravseth <atiravsethwork@gmail.com>
2026-04-07 19:28:44 -0400

    temp

--- TBE_controller/data_obtainer.py
+++ TBE_controller/data_obtainer.py
@@ -131,7 +131,7 @@ class SensorData():
 
         arb_id = (MODE_MIT << 8) | MOTOR_ID
         msg = can.Message(arbitration_id=arb_id, data=buf, is_extended_id=True)
-        self.can_bus.send(msg)
+        # self.can_bus.send(msg)
   
         # Reseting the torque value to zero
         self.torque_input = 0.0
