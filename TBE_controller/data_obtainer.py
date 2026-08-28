a5e84ce3afea963288c1b581b5f150e434b14d31
gautambalaji02 <gautambalaji02@gmail.com>
2026-08-28 16:03:47 -0400

    Added demo file

--- TBE_controller/data_obtainer.py
+++ TBE_controller/data_obtainer.py
@@ -185,7 +185,7 @@ class SensorData():
 
         arb_id = (MODE_MIT << 8) | MOTOR_ID
         msg = can.Message(arbitration_id=arb_id, data=buf, is_extended_id=True)
-        # self.can_bus.send(msg)
+        self.can_bus.send(msg)
 
         # Getting feedback
         feedback = self.read_feedback()
